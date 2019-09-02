0f8590c34dfe288d6f6b7c19bbe89bb6a2026f25
DryanLam <tandunglam@gmail.com>
2019-09-02 16:48:31 +0700

    Added authen load

--- Components/Page/login/action.js
+++ Components/Page/login/action.js
@@ -24,6 +24,7 @@ function oAuthRC(){
 	
 	if (obj.username == "admin" && obj.password == "21232F297A57A5A743894A0E4A801FC3"){
 		alert ("Login successfully");
+		sessionStorage.isAuthen = true;
 		redirectUrl();
 	}else{
 		FAILED_TIMES--;
